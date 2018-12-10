7e1a112a2c92d0c70ba02c28b8b88bdf0587d249
Melanie Backers <melanie.backers@hva.nl>
2018-12-10 12:41:56 +0100

    overview page fixed

--- AppAENS/app/src/main/java/com/example/melanie/appaens/fragment/OverviewFragment.java
+++ AppAENS/app/src/main/java/com/example/melanie/appaens/fragment/OverviewFragment.java
@@ -7,8 +7,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.melanie.appaens.R;
+import com.example.melanie.appaens.data.DataSource;
+import com.example.melanie.appaens.model.Question;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +21,19 @@ import com.example.melanie.appaens.R;
 public class OverviewFragment extends Fragment {
 
     private Context context;
+    private List<Question> questionList;
+
+    private TextView mAantalRood;
+    private TextView mAantalOranje;
+    private TextView mAantalGeel;
+    private TextView mAantalBlauw;
+    private TextView mAantalGroen;
+    private TextView mAantalRoze;
+    private TextView mAantalPaars;
+
+    public OverviewFragment(){
+
+    }
 
     public OverviewFragment(Context context) {
         this.context = context;
@@ -29,7 +47,37 @@ public class OverviewFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_overview, container, false);
         view.setTag("OVERVIEWFRAGMENT");
 
+        DataSource data = new DataSource();
+        questionList = data.getQuestionList();
+
+        mAantalRood = (TextView) view.findViewById(R.id.idOverview_aantalRood);
+        mAantalOranje = (TextView) view.findViewById(R.id.idOverview_aantalOranje);
+        mAantalGeel = (TextView) view.findViewById(R.id.idOverview_aantalGeel);
+        mAantalBlauw = (TextView) view.findViewById(R.id.idOverview_aantalBlauw);
+        mAantalGroen = (TextView) view.findViewById(R.id.idOverview_aantalGroen);
+        mAantalRoze = (TextView) view.findViewById(R.id.idOverview_aantalRoze);
+        mAantalPaars = (TextView) view.findViewById(R.id.idOverview_aantalPaars);
+        
+        mAantalRood.setText("" + getAantal(1));
+        mAantalOranje.setText("" + getAantal(2));
+        mAantalGeel.setText("" + getAantal(3));
+        mAantalBlauw.setText("" + getAantal(4));
+        mAantalGroen.setText("" + getAantal(5));
+        mAantalRoze.setText("" + getAantal(6));
+        mAantalPaars.setText("" + getAantal(7));
+
         return view;
     }
 
+    private int getAantal(int kleurid){
+        int aantal = 0;
+
+        for (Question q : questionList){
+            if (q.getAnswerClient() == kleurid){
+                aantal++;
+            }
+        }
+
+        return aantal;
+    }
 }
