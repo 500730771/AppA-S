c923542ef83a4a4bf9c77cb9c2a597d602c0e36c
Melanie Backers <melanie.backers@hva.nl>
2018-12-12 13:54:07 +0100

    bedanktactivity en fragment_overview show again

--- AppAENS/app/src/main/java/com/example/melanie/appaens/activity/BedanktActivity.java
+++ AppAENS/app/src/main/java/com/example/melanie/appaens/activity/BedanktActivity.java
@@ -1,12 +1,33 @@
 package com.example.melanie.appaens.activity;
 
+import android.graphics.Bitmap;
+import android.graphics.Path;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.graphics.pdf.PdfDocument;
+import android.media.Image;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.print.PrintAttributes;
 import android.print.pdf.PrintedPdfDocument;
+import android.support.constraint.solver.widgets.Rectangle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.example.melanie.appaens.R;
+
+import org.w3c.dom.Document;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import static android.print.PrintAttributes.CREATOR;
 
 public class BedanktActivity extends AppCompatActivity {
 
@@ -16,31 +37,9 @@ public class BedanktActivity extends AppCompatActivity {
         setContentView(R.layout.activity_bedankt);
 
 
+        String filename = "emptyPDF.pdf";
 
-
-
-
-//        // open a new document
-//        PrintedPdfDocument document = new PrintedPdfDocument(this, printAttributes);
-//
-//        // start a page
-//        PdfDocument.Page page = document.startPage(0);
-//
-//        // draw something on the page
-//        View content = getContentView();
-//        content.draw(page.getCanvas());
-//
-//        // finish the page
-//        document.finishPage(page);
-//
-//        // add more pages
-//
-//        // write the document content
-//        document.writeTo(getOutputStream());
-//
-//        //close the document
-//        document.close();
-
+//        PDDocument doc = new PDDocument();
 
     }
 }
