ce6f393bac9268463bee85bf4c77432e570c6949
Melanie Backers <melanie.backers@hva.nl>
2018-12-19 22:41:24 +0100

    constraint informatiebalk

--- AppAENS/app/src/main/java/com/example/melanie/appaens/activity/ScrollMenuActivity.java
+++ AppAENS/app/src/main/java/com/example/melanie/appaens/activity/ScrollMenuActivity.java
@@ -1,14 +1,16 @@
 package com.example.melanie.appaens.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.example.melanie.appaens.R;
 import com.example.melanie.appaens.adapter.RecyclerViewAdapter;
-import com.example.melanie.appaens.adapter.RecyclerViewInformatieAdapter;
 import com.example.melanie.appaens.data.DataSource;
 import com.example.melanie.appaens.fragment.HeaderFragment;
 import com.example.melanie.appaens.fragment.QuestionFragment;
@@ -47,7 +49,74 @@ public class ScrollMenuActivity extends AppCompatActivity {
         activity.getSupportFragmentManager().beginTransaction().replace(R.id.question_fragment, qfragment).addToBackStack(null).commit();
 
         initRecyclerviewNavigation();
-        initRecyclerviewInformatie();
+
+        LinearLayout mBoxred = (LinearLayout) findViewById(R.id.idInformatie_boxred);
+        LinearLayout mBoxorange = (LinearLayout) findViewById(R.id.idInformatie_boxorange);
+        LinearLayout mBoxyellow = (LinearLayout) findViewById(R.id.idInformatie_boxyellow);
+        LinearLayout mBoxblue = (LinearLayout) findViewById(R.id.idInformatie_boxblue);
+        LinearLayout mBoxgreen = (LinearLayout) findViewById(R.id.idInformatie_boxgreen);
+        LinearLayout mBoxpink = (LinearLayout) findViewById(R.id.idInformatie_boxpink);
+        LinearLayout mBoxpurple = (LinearLayout) findViewById(R.id.idInformatie_boxpurple);
+
+        mBoxred.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollMenuActivity.this, Popup.class);
+                intent.putExtra("Informatie", mInformatieList.get(0));
+                startActivity(intent);
+                overridePendingTransition(R.anim.from_below, R.anim.to_below);
+            }
+        });
+        mBoxorange.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollMenuActivity.this, Popup.class);
+                intent.putExtra("Informatie", mInformatieList.get(1));
+                startActivity(intent);
+                overridePendingTransition(R.anim.from_below, R.anim.to_below);
+            }
+        });
+        mBoxyellow.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollMenuActivity.this, Popup.class);
+                intent.putExtra("Informatie", mInformatieList.get(2));
+                startActivity(intent);
+                overridePendingTransition(R.anim.from_below, R.anim.to_below);
+            }
+        });
+        mBoxblue.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollMenuActivity.this, Popup.class);
+                intent.putExtra("Informatie", mInformatieList.get(3));
+                startActivity(intent);overridePendingTransition(R.anim.from_below, R.anim.to_below);
+            }
+        });
+        mBoxgreen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollMenuActivity.this, Popup.class);
+                intent.putExtra("Informatie", mInformatieList.get(4));
+                startActivity(intent);overridePendingTransition(R.anim.from_below, R.anim.to_below);
+            }
+        });
+        mBoxpink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollMenuActivity.this, Popup.class);
+                intent.putExtra("Informatie", mInformatieList.get(5));
+                startActivity(intent);overridePendingTransition(R.anim.from_below, R.anim.to_below);
+            }
+        });
+        mBoxpurple.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ScrollMenuActivity.this, Popup.class);
+                intent.putExtra("Informatie", mInformatieList.get(6));
+                startActivity(intent);overridePendingTransition(R.anim.from_below, R.anim.to_below);
+            }
+        });
     }
 
     public void initRecyclerviewNavigation(){
@@ -55,14 +124,6 @@ public class ScrollMenuActivity extends AppCompatActivity {
         RecyclerView rv = (RecyclerView) findViewById(R.id.idMenu_NavigationList);
         rv.setLayoutManager(layoutManager);
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mCategorieList, mDrawableList);
-        rv.setAdapter(adapter);
-    }
-
-    private void initRecyclerviewInformatie(){
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
-        RecyclerView rv = (RecyclerView) findViewById(R.id.idMenu_InformationList);
-        rv.setLayoutManager(layoutManager);
-        RecyclerViewInformatieAdapter adapter = new RecyclerViewInformatieAdapter(this, mInformatieList, mColorList);
         rv.setAdapter(adapter);
     }
 
