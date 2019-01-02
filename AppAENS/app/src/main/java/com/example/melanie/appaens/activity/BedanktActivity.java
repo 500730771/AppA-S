b111c71a59a4439f8c54ed91047b5d7d6446880c
Beerentvos <Beerent.vos@gmail.com>
2019-01-02 13:49:45 +0100

    Update BedanktActivity.java

--- AppAENS/app/src/main/java/com/example/melanie/appaens/activity/BedanktActivity.java
+++ AppAENS/app/src/main/java/com/example/melanie/appaens/activity/BedanktActivity.java
@@ -31,7 +31,7 @@ public class BedanktActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bedankt);
         ActionBar toolbar = getSupportActionBar();
         toolbar.setTitle("Bedankt");
 
